f20f2a4f390bf9cbb15fd70a42055eb88eb531a0
Rida-Shahid <114757576+Rida-Shahid@users.noreply.github.com>
2023-12-19 21:22:24 +0500

    Time: 32 ms (89.02%), Space: 16.1 MB (78.69%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -0,0 +1,10 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        s=s.lower()
+        return s
+
+
+
+        #just to maintain my streak as Im not in my mood to think 
+        #worst approach 
+        
